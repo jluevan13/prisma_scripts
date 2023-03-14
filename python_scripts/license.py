d2c15c353c40ab592c47cc69be75e81efd7a6604
jluevan13 <luevano.jesus13@gmail.com>
2023-03-14 15:12:29 -0700

    update output format

--- python_scripts/license.py
+++ python_scripts/license.py
@@ -58,9 +58,13 @@ def get_credit_usage(group_id, time, unit):
         "x-redlock-auth": token,
     }
     response = requests.request("POST", url, headers=headers, data=usage_payload_json)
+    # print(response.json())
     return response.json()
 
 
 for group in account_groups.json():
     usage_by_group = get_credit_usage(group["id"], "1", "month")
-    print(f'{group["id"]} ({group["name"]}): {usage_by_group["stats"]["total"]}')
+    for account in usage_by_group["items"]:
+        print(
+            f"{group['name']}, {group['id']}, {account['account']['name']}, {account['account']['id']}, {account['total']}"
+        )
