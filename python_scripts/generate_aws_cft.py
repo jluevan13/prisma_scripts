0d49f65c2357b6b8cafe52b9a58112d4cdb649c6
jluevan13 <luevano.jesus13@gmail.com>
2023-03-20 13:33:11 -0400

    update to use presigned s3 url

--- python_scripts/generate_aws_cft.py
+++ python_scripts/generate_aws_cft.py
@@ -3,32 +3,47 @@ import json
 import os
 import boto3
 from botocore.exceptions import ClientError
+from urllib.parse import unquote
 
 api = "api4"  # set api based on tenant (api3, api2, api)
+account_id = os.environ.get("awsAccount")
+root_ou = os.environ.get("rootOU")  # root ou id
+cf_stack_arn = os.environ.get("cfStackId")  # cloudformation stack name or id
+prisma_role_name = "PrismaCloudRole"  # update with prisma cloud role name that is defined in prisma console
+
+### Set up aws credentials
+session = boto3.session.Session(
+    aws_access_key_id=os.environ.get("awsAccessKey"),
+    aws_secret_access_key=os.environ.get("awsSecretKey"),
+    region_name="us-east-1",
+    profile_name="security",
+)
+
+cf_client = session.client("cloudformation")
+
 
 ### Create Auth Token for tenant
 login_url = f"https://{api}.prismacloud.io/login"
 
-payload = {}
-payload["username"] = os.environ.get("prismaUserName")
-payload["password"] = os.environ.get("prismaSecretKey")
-payload_json = json.dumps(payload)
+login_payload = {}
+login_payload["username"] = os.environ.get("prismaUserName")
+login_payload["password"] = os.environ.get("prismaSecretKey")
+payload_json = json.dumps(login_payload)
 headers = {
     "Content-Type": "application/json; charset=UTF-8",
     "Accept": "application/json; charset=UTF-8",
 }
 
 auth_token = requests.request("POST", login_url, headers=headers, data=payload_json)
-auth_token_data = auth_token.json()
-token = auth_token_data["token"]
-
+token = auth_token.json()["token"]
 
 ### Get supported features
+### Supported features for aws organizations are listed below
 features_url = f"https://{api}.prismacloud.io/cas/v1/features/cloud/aws"
 
 supported_features_payload = json.dumps(
     {
-        "accountType": "organization",  # change this to account for a individual account not in an organization
+        "accountType": "organization",  # change this to account for an individual account not in an organization
         "awsPartition": "us-east-1",
         "rootSyncEnabled": True,
     }
@@ -47,12 +62,12 @@ supported_features = requests.request(
 # Supported Features -> 'supportedFeatures': ['Agentless Scanning', 'Auto Protect', 'Cloud Visibility Compliance and Governance', 'Remediation', 'Serverless Function Scanning']
 
 
-## Generate CFT
-url = f"https://{api}.prismacloud.io/cas/v1/aws_template"
-account_id = os.environ.get("awsAccount")
-generate_cft_payload = json.dumps(
+# Generate s3 presigned url with template
+template_url = f"https://{api}.prismacloud.io/cas/v1/aws_template/presigned_url"
+
+template_payload = json.dumps(
     {
-        "accountType": "organization",
+        "accountType": "organization",  # change this to account for an individual account not in an organization
         "accountId": account_id,
         "awsPartition": "us-east-1",
         "features": [
@@ -64,40 +79,26 @@ generate_cft_payload = json.dumps(
         ],
     }
 )
-headers = {"Content-Type": "application/json", "x-redlock-auth": token}
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "x-redlock-auth": token,
+}
 
-cft = requests.request("POST", url, headers=headers, data=generate_cft_payload)
-
-with open("prisma_org_role.template", "w") as f:
-    f.write(cft.text)
-
-session = boto3.session.Session(
-    aws_access_key_id=os.environ.get("awsAccessKey"),
-    aws_secret_access_key=os.environ.get("awsSecretKey"),
-    region_name="us-east-1",
-    profile_name="security",
+template_response = requests.request(
+    "POST", template_url, headers=headers, data=template_payload
 )
 
-cf_client = session.client("cloudformation")
-s3_client = session.resource("s3")
 
-bucket_name = os.environ.get("prismaBucketName")
-object_key = "prisma_org_role.template"
-
-s3_client.meta.client.upload_file(
-    "prisma_org_role.template",  # path to file
-    bucket_name,
-    object_key,
-)
-
-root_ou = os.environ.get("rootOU")
-cf_stack_arn = os.environ.get("cfStackId")
-prisma_role_name = "PrismaCloudRole"  # update with prisma cloud role name that is defined in prisma console
-
+### Update the stack with the new template
 try:
     response = cf_client.update_stack(
         StackName=cf_stack_arn,
-        TemplateURL=f"https://{bucket_name}.s3.amazonaws.com/{object_key}",
+        TemplateURL=unquote(
+            template_response.json()["createStackLinkWithS3PresignedUrl"].split(
+                "templateURL="
+            )[1]
+        ),
         Parameters=[
             {"ParameterKey": "OrganizationalUnitIds", "ParameterValue": root_ou},
             {"ParameterKey": "PrismaCloudRoleName", "ParameterValue": prisma_role_name},
